6b098ce7e361faa6a41af964d1f884d1606c5184
Zaiste <oh@zaiste.net>
2023-01-05 10:54:56 +0100

    top-level await + Node.js 18 requirement

--- app.ts
+++ app.ts
@@ -1,25 +1,13 @@
 import { ServerApp } from 'retes';
 import { Response } from 'retes/response';
-import { GET, POST } from 'retes/route';
+import { GET } from 'retes/route';
 
 const routes = [
 	GET('/', () => Response.OK('Hello, World')),
-	GET('/welcome/:name', ({ params }) => {
-		return { status: 200, body: `Hello, ${params.name}` };
-	}),
-	POST('/user', ({ params: { name } }) => Response.OK(`Received: '${name}'`)),
-	POST('/widget', ({ params: { name, count } }) => {
-		// validate `params`
-		// save the widget to database ...
-		return Response.Created(); // returns `201 Created` response
-	}),
 ];
 
-async function main() {
-	const app = new ServerApp(routes);
-	await app.start(3000);
+const app = new ServerApp(routes);
+await app.start(3000);
 
-	console.log('Started');
-}
+console.log('Server started on port 3000');
 
-main();
